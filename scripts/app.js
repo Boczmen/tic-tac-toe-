5bd7072a11e9829ff5b2e22736bffbffe87aa81a
Boczers <Marchewa626@gmail.com>
2023-05-17 19:57:36 +0200

    dodane pole konfiguracji

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,22 @@
-const editPlayerButton1 = document.getElementById('edit-player-1-btn')
-const editPlayerButton2 = document.getElementById('edit-player-2-btn')
+const playerConfigOverlay = document.getElementById("config-overlay"); //pole konfiguracja
+const backDropElement = document.getElementById("backdrop"); //przyciemnione tło konfiguracji
+const configForm = document.getElementById("playername-config"); // forma, input, label dla konfiguracji
 
-editPlayerButton1.addEventListener('click', openPlayerConfig)
-editPlayerButton2.addEventListener('click', openPlayerConfig)
+const editPlayerButton1 = document.getElementById("edit-player-1-btn"); //przycisk edit 1
+const editPlayerButton2 = document.getElementById("edit-player-2-btn"); //przycisk edit 2
+
+const cancelConfigButton = document.getElementById("button-alt"); // przycisk wyłączający konfigurację
+
+const playerNameInput = document.getElementById("playername"); //pole do wpisania nazwy gracza
+const confirmButton = document.getElementById("confirm-button"); //przycisk potwierdzający nazwę gracza
+
+const player1Name = document.getElementById("player-1-name"); //nazwa gracza 1
+const player2Name = document.getElementById("player-2-name"); //nazwa gracza 2
+
+editPlayerButton1.addEventListener("click", openPlayerConfig);
+editPlayerButton2.addEventListener("click", openPlayerConfig);
+
+cancelConfigButton.addEventListener("click", closeConfiguration);
+backDropElement.addEventListener("click", closeConfiguration);
+
+configForm.addEventListener("submit", changePlayerName);
--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,19 @@
-function openPlayerConfig(){
+function openPlayerConfig() {
+  playerConfigOverlay.style.display = "block";
+  backDropElement.style.display = "block";
+}
 
-console.log(editPlayerButton1)
+function closeConfiguration() {
+  playerConfigOverlay.style.display = "none";
+  backDropElement.style.display = "none";
+}
 
+function changePlayerName(event) {
+  //   const configInput = EventTarget.textContent
+
+  event.preventDefault();
+const formData = new FormData(event.target)
+const enteredPlayername = formData.get('playername')
+
+enteredPlayername = player1Name.textContent
 }
